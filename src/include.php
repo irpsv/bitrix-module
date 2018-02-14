9eea54a4c2fa3c8703fa4d8bdd0b82681c80ead3
irpsv <i.rpsv@live.com>
2018-02-14 22:54:30 +0500

    Update include.php

--- src/include.php
+++ src/include.php
@@ -8,7 +8,6 @@ include_once __DIR__.'/autoload.php';
 
 $loader = new Psr4AutoloaderClass();
 $loader->addNamespace('module_id', __DIR__.'/lib');
-$loader->addNamespace('module_id\components', __DIR__.'/components');
 $loader->register();
 
 //
