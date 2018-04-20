fcecc2f4197f43acb9f61dcf5d202d954d8ce1e9
irpsv <i.rpsv@live.com>
2018-04-20 12:26:57 +0500

    add components

--- src/components/test-comp/class.php
+++ src/components/test-comp/class.php
@@ -2,10 +2,29 @@
 
 namespace module_id\components\classes;
 
+\Bitrix\Main\Localization\Loc::loadMessages(__FILE__);
+
 class TestComp extends \CBitrixComponent
 {
 	public function executeComponent()
 	{
-		// code
+		// кеширование
+		$cacheTime = $this->arParams['CACHE_TIME'] ?? 3600;
+		$cacheAdditionalId = null;
+		$cachePath = preg_replace('/[^a-z0-9]/i', '_', __CLASS__);
+		if ($this->startResultCache($cacheTime, $cacheAdditionalId, $cachePath)) {
+			$this->run();
+		}
+	}
+
+	public function run()
+	{
+		// получение языковых сообщенте
+		$message = \GetMessage('testMessage');
+		
+		// основная логика работы компонета
+		$param1 = $this->arParams['key'];
+		$this->arResult['key'] = "значение которое улетит в шаблон";
+		$this->includeComponentTemplate();
 	}
 }
