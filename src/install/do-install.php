1779fc5e7094d316080177d3f10c620a2b3723e8
irpsv <i.rpsv@live.com>
2018-08-02 19:40:14 +0500

    add ModuleIblockBuilder

--- src/install/do-install.php
+++ src/install/do-install.php
@@ -1,111 +1,139 @@
 <?php
 
-\CModule::includeModule('iblock');
+namespace module_id\install;
 
-//
-// iblock type
-//
-$iblockTypeId = "module_id_preffix";
-if (\CIBlockType::getById($iblockTypeId)->selectedRowsCount() < 1) {
-	$iblockType = new \CIBlockType;
-	$iblockType->Add([
-		'ID' => $iblockTypeId,
-		'SECTIONS' => 'Y',
-		'IN_RSS' => 'N',
-		'SORT' => '500',
-		'LANG' => [
-			'ru' => [
-				'NAME' => '',
-				'SECTION_NAME' => 'Разделы',
-				'ELEMENT_NAME' => 'Элементы',
+include_once __DIR__.'/ModuleIblockBuilder.php';
+
+class Test extends \ModuleIblockBuilder
+{
+	/**
+	 * https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblocktype/add.php
+	 */
+	public function getType()
+	{
+		return [
+			'ID' => 'type',
+			'SECTIONS' => 'Y',
+			'IN_RSS' => 'N',
+			'SORT' => '500',
+			'LANG' => [
+				'ru' => [
+					'NAME' => 'Название',
+					'SECTION_NAME' => 'Разделы',
+					'ELEMENT_NAME' => 'Элементы',
+				],
 			],
-		],
-	]);
-	if ($iblockType->LAST_ERROR) {
-		throw new \Exception($iblockType->LAST_ERROR);
+		];
+	}
+
+	/**
+	 * https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblock/add.php
+	 * Подставляются автоматически:
+	 * LID
+	 * IBLOCK_TYPE_ID
+	 */
+	public function getIblock()
+	{
+		return [
+			'NAME' => 'Название',
+			'CODE' => 'Символьный код', // обязательно
+			'BIZPROC' => 'N',
+			'WORKFLOW' => 'N',
+		];
+	}
+
+	/**
+	 * https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblockproperty/add.php
+	 * Подставляются автоматически:
+	 * IBLOCK_ID
+	 */
+	public function getProperties()
+	{
+		return [
+			[
+				'NAME' => 'Название',
+				'CODE' => 'property_code_1', // обязательно
+				'PROPERTY_TYPE' => 'S',
+				// 'PROPERTY_TYPE' => 'N',
+				// 'PROPERTY_TYPE' => 'F',
+				// 'PROPERTY_TYPE' => 'E',
+				// 'PROPERTY_TYPE' => 'G',
+			],
+			[
+				'NAME' => 'Название',
+				'CODE' => 'property_code_2', // обязательно
+				'PROPERTY_TYPE' => 'L',
+				'VALUES' => [
+					[
+						'VALUE' => 'Значение списочного свойства 1',
+					],
+					[
+						'VALUE' => 'Значение списочного свойства 2',
+						'SORT' => 100,
+						'DEF' => 'Y',
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblocksection/add.php
+	 * Подставляются автоматически:
+	 * IBLOCK_ID
+	 */
+	public function getSections()
+	{
+		return [
+			[
+				'NAME' => 'Название категории 1',
+			],
+			[
+				'NAME' => 'Название категории 2',
+				'CHILDRENS' => [
+					[
+						'NAME' => 'Название дочерней категории 1',
+						'CODE' => 'Символьный код',
+					],
+					[
+						'NAME' => 'Название дочерней категории 2',
+						'CHILDRENS' => [
+							[
+								'NAME' => 'Название дочерней дочки категории',
+							],
+						],
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblockelement/add.php
+	 * Подставляются автоматически:
+	 * IBLOCK_ID
+	 */
+	public function getElements()
+	{
+		return [
+			[
+				'NAME' => 'Название',
+				'PROPERTY_VALUES' => [
+					'property_code_1' => 'Значение',
+					'property_code_2' => $this->getPropertyValueIdByValue("property_code_2", "Значение списочного свойства 1"), // для списочных
+				],
+			],
+			[
+				'NAME' => 'Название',
+				'IBLOCK_SECTION_ID' => $this->getSectionIdByName("Название категории 1"),
+				'SECTIONS' => [
+					"Название категории 1",
+					"Название категории 2",
+					"Название дочерней дочки категории",
+				],
+			],
+		];
 	}
 }
 
-//
-// iblock
-//
-$iblockCode = "module_id_preffix";
-$filter = [
-	'CODE' => $iblockCode,
-	'IBLOCK_TYPE_ID' => $iblockTypeId,
-];
-$iblock = \CIBlock::getList([], $filter)->fetch();
-if ($iblock) {
-	$iblockId = $iblock['ID'];
-}
-else {
-	$iblock = new \CIBlock;
-	$iblockId = $iblock->Add([
-		'LID' => \SITE_ID,
-		'NAME' => '',
-		'CODE' => $iblockCode,
-		'IBLOCK_TYPE_ID' => $iblockTypeId,
-		'BIZPROC' => 'N',
-		'WORKFLOW' => 'N',
-	]);
-	if ($iblock->LAST_ERROR) {
-		throw new \Exception($iblock->LAST_ERROR);
-	}
-}
-
-//
-// iblock property
-//
-$propertyCode = "module_id_preffix";
-$filter = [
-	'CODE' => $propertyCode,
-	'IBLOCK_ID' => $iblockId,
-];
-$property = \CIBlockProperty::getList([], $filter)->fetch();
-if ($property) {
-	$propertyId = $property['ID'];
-}
-else {
-	$property = new \CIBlockProperty;
-	$propertyId = $property->Add([
-		'NAME' => '',
-		'CODE' => $propertyCode,
-		'IBLOCK_ID' => $iblockId,
-		'PROPERTY_TYPE' => 'S',
-		'PROPERTY_TYPE' => 'N',
-		'PROPERTY_TYPE' => 'F',
-		'PROPERTY_TYPE' => 'E',
-		'PROPERTY_TYPE' => 'G',
-		// 'USER_TYPE' => 'user_type',
-		// 'USER_TYPE_SETTINGS' => serialize([]),
-	]);
-	if ($property->LAST_ERROR) {
-		throw new \Exception($property->LAST_ERROR);
-	}
-}
-
-//
-// iblock property enum
-//
-$proprtyValues = [
-	"раз",
-	"2",
-	"three",
-];
-foreach ($proprtyValues as $propertyValue) {
-	$filter = [
-		'VALUE' => $propertyValue,
-		'PROPERTY_ID' => $propertyId,
-	];
-	$count = \CIBlockPropertyEnum::getList([], $filter, []);
-	if ($count == 0) {
-		$row = new \CIBlockPropertyEnum;
-		$row->Add([
-			'PROPERTY_ID' => $propertyId,
-			'VALUE' => $propertyValue,
-		]);
-		if ($row->LAST_ERROR) {
-			throw new \Exception($row->LAST_ERROR);
-		}
-	}
-}
+(new Test)->create();
