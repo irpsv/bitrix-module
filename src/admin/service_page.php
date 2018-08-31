b9d9ab3a5a749f518dcc21511b297ddaadbe5561
irpsv <i.rpsv@live.com>
2018-08-31 15:43:01 +0500

    update admin pages

---
+++ src/admin/service_page.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php";
+
+$moduleId = "moduleId";
+if ($APPLICATION->GetGroupRight($moduleId) == "D") {
+    $APPLICATION->AuthForm("Не достаточно прав");
+}
+$APPLICATION->SetTitle("Заголовок");
+
+$errors = [];
+$success = [];
+
+//
+// process code
+//
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_after.php";
+
+if ($errors) {
+    CAdminMessage::ShowMessage([
+        'MESSAGE' => 'В ходе работы возникли ошибки',
+        'TYPE' => 'ERROR',
+        'DETAILS' => join("<br>", $errors),
+    ]);
+}
+if ($success) {
+    CAdminMessage::ShowMessage([
+        'MESSAGE' => 'Сохранение прошло успешно',
+        'TYPE' => 'OK',
+        'DETAILS' => join("<br>", $success),
+    ]);
+}
+
+require $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_admin.php";
